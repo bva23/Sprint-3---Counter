ab8c9b0a856ef489f349fccb6d1678bc11bf449b
Vladimir Bogomolov <bva.mos@yandex.ru>
2023-02-21 10:43:42 +0300

    Создан счетчик.

--- Sprint 3 - Counter/ViewController.swift
+++ Sprint 3 - Counter/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var labelForCounter: UILabel!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        labelForCounter.text = "0"
+        
     }
-
-
+    
+    @IBAction func buttonDidTap(_ sender: Any) {
+        let counter: Int = Int(labelForCounter.text!)! + 1
+        labelForCounter.text = String(counter)
+    }
 }
 
